da0bbd1f2280345d6739e70201a87141c44b8e54
journeyhancock <journeyhancock480@gmail.com>
2025-10-29 11:19:46 -0700

    feat: make base controller only one controller and one firmware

--- BaseController/base_controller.py
+++ BaseController/base_controller.py
@@ -5,12 +5,13 @@ from typing import List
 
 class BaseController:
     def __init__(self):
-        self.controllers: List[Controller] = []
-        self.firmwares: List[Firmware] = [Firmware(
+        self.controller: Controller
+        self.firmware: Firmware
+
+        self.controller = create_controllers.flight_computer_rev2_controller()
+        self.firmware = Firmware(
             id=b"\x06",
             name="APPA",
             preset_frame_size=0,
             preset_file=""
-        )]
-
-        self.controllers.append(create_controllers.flight_computer_rev2_controller())
+        )
