e282f775e788aea5613769a0f59135ea377b5504
[Peter Trakas] <[petertrakas1@gmail.com]>
2025-10-31 15:50:00 -0500

    rough outline of base sensor

--- Sensor/sensor.py
+++ Sensor/sensor.py
@@ -1,13 +1,54 @@
 from typing import Callable
 
 class Sensor:
-    def __init__(self, name: str, unit: str, convert_data: Callable[[float], float]):
+    def __init__( self, name: str, unit: str, convert_data: Callable[[float], float,], low_byte_range : int, upp_byte_range : int) :
         self.name = name
         self.unit = unit
         self.convert_data = convert_data
+        self.low_byte_range = low_byte_range
+        self.upp_byte_range = upp_byte_range
+    
+    def convert_data( self ) -> float:
+        return self.conv_data
         
-    def data_dump(self) -> float:
+    def data_dump( self, opcode, subcommand ) -> float:
         # TODO:
+        #check appa.py for list of sensors, substring for dump
+        #check hardwarecommand lines 341, 571,
+        #commands for opcode
+        
+        #set command opcode
+        #opcode = b'\x03'
+        
+        #send command opcode
+        serialObj.sendByte( opcode )
+        
+        #send sensor dump subcommand code b'\x01' for all sensors
+        serialObj.sendByte( subcommand )
+        
+        
+        #toal byte size of sensors is 28 bytes
+        #sensor_dump_size_bytes = 28
+        sensor_dump_size_bytes = serialObj.readByte()
+        sensor_dump_size_bytes = int.from_bytes(sensor_dump_size_bytes, "big")
+        
+        #intiliaze array to hold sensor dump bytes
+        sensor_bytes_list = [] #should this be passed as a parameter?
+        
+
+        
+         #sensor readout goes from bytes to int to unit integer
+        
+        #append bytes to byte list
+        for i in range(sensor_dump_size_bytes):
+            sensor_bytes_list.append( serialObj.readByte() )    
+            
+        
+        data_bytes = sensor_bytes_list[ self.low_byte_range : self.upp_byte_range ]
+        
+        #still need to format
+        
+        
         # send command opcode
         # send sensor dump code
         # read a byte to get the size of sensor dump
@@ -18,3 +59,5 @@ class Sensor:
 
     def poll(self) -> None:
         pass
+    
+    
--- Testing/test_sensor.py
+++ Testing/test_sensor.py
@@ -1,2 +1,36 @@
-from Sensor import Sensor
+from Sensor.sensor import *
 
+def conv_fucntion(raw_data: float) -> float:
+    return raw_data * 2.0 # Example conversion logic
+
+sensor = Sensor(
+    name = "Test Sensor",
+    unit = "units",
+    convert_data = conv_fucntion,
+    low_byte_range = 4, # example byte range for accelerometer z
+    upp_byte_range = 6
+)
+
+print(f"Sensor Name: {sensor.name}")
+print(f"Sensor Unit: {sensor.unit}")
+#convert data example call
+print(f"Converted Data: {sensor.convert_data(10.0)}")  # Example raw data input
+#data_dump example call, does not work because of missing serialObj, unsure how to handle that
+print(f"Data Dump: {sensor.data_dump(b'\\x03', b'\\x01')}")  # Example opcode and subcommand
+
+"""
+    appa_sensor_sizes = {
+    "raw": {
+        "accX" :         2,
+        "accY" :         2,
+        "accZ" :         2,
+        "gyroX":         2,
+        "gyroY":         2,
+        "gyroZ":         2,
+        "magX" :         2,
+        "magY" :         2,
+        "magZ" :         2,
+        "imut" :         2,
+        "pres" :         4,
+        "temp" :         4
+        """
