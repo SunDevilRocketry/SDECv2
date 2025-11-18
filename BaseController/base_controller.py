cfaaf0c690d25ac00de1809134811e22d1c6ad28
journeyhancock <journeyhancock480@gmail.com>
2025-11-17 23:12:41 -0700

    fix: make BaseController init take a Controller parameter

--- BaseController/base_controller.py
+++ BaseController/base_controller.py
@@ -4,8 +4,8 @@ from .sensor import BaseSensor
 from typing import List, Callable
 
 class BaseController:
-    def __init__(self, controller_func: Callable[[], Controller], firmware: Firmware):
-        self.controller: Controller = controller_func()
+    def __init__(self, controller: Controller, firmware: Firmware):
+        self.controller: Controller = controller
         self.firmware: Firmware = firmware
 
     def __str__(self):
