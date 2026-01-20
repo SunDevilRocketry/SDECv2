526634785422d508bdf5645e8a64a40ddae16321
Journey Hancock <journeyhancock480@gmail.com>
2026-01-20 02:36:52 -0700

    misc: update sdecv2 __init__.py imports

--- __init__.py
+++ __init__.py
@@ -2,3 +2,7 @@
 # Copyright (c) 2025 Sun Devil Rocketry
 
 # make Python recognize SDECv2 as a package
+from .BaseController import BaseController, BaseSensor, Controller, Firmware, create_controllers
+from .Parser import Parser, Preset, PresetConfig, Feature, Bitmask, Data, create_configs
+from .Sensor import Sensor, SensorSentry, create_sensors, util
+from .SerialController import SerialObj, SerialSentry, Comport
