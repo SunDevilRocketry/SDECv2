e403b5141048d56e9b97940ed8008761debcc25d
journeyhancock <journeyhancock480@gmail.com>
2026-01-22 15:38:28 -0700

    feat: untested flash extract

--- Parser/parser.py
+++ Parser/parser.py
@@ -1,12 +1,18 @@
+import struct
+
+from .bitmask import Bitmask
 from .data import Data
 from .feature import Feature
 from .preset import Preset
+from SerialController import SerialObj
 from typing import List
+
+FLASH_SIZE = 524288
 
 class Parser:
     def __init__(self, 
                  preset: Preset, 
-                 data_bitmask: bytes, 
+                 data_bitmask: Bitmask, 
                  features: List[Feature], 
                  sensor_frame_size: int, 
                  num_frames: int):
@@ -17,13 +23,73 @@ class Parser:
         self.num_frames = num_frames
     
     def download_preset(self) -> Data:
-        return Data({})
+        pass
     
     def verify_preset(self) -> Data:
-        return Data({})
+        pass
     
     def upload_preset(self) -> Data:    
-        return Data({})
+        pass
     
-    def flash_extract_parse(self) -> Data:
-        return Data({})
+    def flash_extract(self, serial_connection: SerialObj) -> List[Data]:
+        flash_data: List[Data] = []
+        # flash opcode
+        serial_connection.send(b"\x22")
+        # flash extract subcommand code
+        serial_connection.send(b"\xC0")
+        # flush buffer ?
+        # start timer ?
+
+        # extract the number of frames in bitmask from flash (4096 blocks max)
+        # each frame reads the amount of bytes the size of the frame
+        # add these to the overall bytes of flash
+        sensor_frame_bytes = b""
+        for i in range(self.num_frames):
+            if (i % 100 == 0): print(f"Reading block {i} ...")
+            # if active roll read an extra 4??? do this better?
+            sensor_frame_bytes += serial_connection.read(self.sensor_frame_size)
+
+        # receive unused bytes amount of bytes
+        serial_connection.read(FLASH_SIZE % self.sensor_frame_size)
+
+        # receive status byte from engine controller (flight computer?)
+        serial_connection.read()
+
+        # get struct format from config preset
+        struct_format = self.preset.config_settings.struct_format
+
+        # parse the bytes, using the presets struct format
+        # verify the adding 2s
+        preset_size = self.preset.config_settings.size
+        preset_bytes = sensor_frame_bytes[2:preset_size + 2]
+        preset_values = struct.unpack(struct_format, preset_bytes)
+        # iterate through the values and put into Data objects
+        features = self.preset.config_settings.enabled_flags.features
+        value_idx = 0
+        for feature in features:
+            feature_data = Data(feature=feature)
+            for sensor in feature.sensors:
+                feature_data.values[sensor] = preset_values[value_idx]
+                value_idx += 1
+            flash_data.append(feature_data)
+
+        # calculate frame size and num preset frames from preset data bitmask
+        # use the size of the preset data bitmask as the sensor frame size
+        # num preset frames is how many preset frames fit in the data size???
+        num_preset_frames = preset_size // self.sensor_frame_size + 1
+        start_idx = num_preset_frames * self.sensor_frame_size
+        stop_idx = start_idx + self.sensor_frame_size
+
+        while stop_idx < FLASH_SIZE:
+            curr_frame_bytes = sensor_frame_bytes[start_idx:stop_idx]
+            curr_frame_values = struct.unpack(struct_format, curr_frame_bytes)
+            # iterate through the values and put into Data objects
+            value_idx = 0
+            for feature in features:
+                feature_data = Data(feature=feature)
+                for sensor in feature.sensors:
+                    feature_data.values[sensor] = curr_frame_values[value_idx]
+                    value_idx += 1
+                flash_data.append(feature_data)
+
+        return flash_data
--- Testing/Parser/test_parser.py
+++ Testing/Parser/test_parser.py
@@ -1,4 +1,5 @@
-from Parser import Preset, PresetConfig, Bitmask, create_configs
+from Parser import Parser, Preset, PresetConfig, Bitmask, create_configs
+from SerialController import SerialObj
 
 def test_preset():
     appa_bitmask = create_configs.appa_bitmask()
@@ -11,6 +12,29 @@ def test_preset():
     appa_preset = Preset(config_settings=appa_preset_config)
 
     print(appa_preset)
+
+def test_flash_extract():
+    serial_connection = SerialObj()
+    serial_connection.init_comport("COM3", 921600, 5)
+    serial_connection.open_comport()
+
+    appa_bitmask = create_configs.appa_bitmask()
+
+    appa_preset_config = PresetConfig(
+        enabled_flags=appa_bitmask,
+        enabled_data=Bitmask(features=[])
+    )
+
+    appa_preset = Preset(config_settings=appa_preset_config)
+
+    # TODO move this to a create function since frame size is a hard value
+    appa_parser = Parser(preset=appa_preset,
+                         data_bitmask=appa_bitmask,
+                         features=appa_bitmask.features,
+                         sensor_frame_size=126,
+                         num_frames=524288 // 126)
+    
+    appa_parser.flash_extract(serial_connection)
 
 if __name__ == "__main__":
     test_preset()
