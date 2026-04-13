12ffd314153a1fbf248d13fb0b8ccfc6f58fdaaf
ETSsound <147210601+ETSsound@users.noreply.github.com>
2026-04-13 15:16:59 -0700

    Support the saving of a controller on connection

--- SerialController/serial_controller.py
+++ SerialController/serial_controller.py
@@ -6,6 +6,7 @@ import serial.tools.list_ports
 
 from .comport import Comport, Status
 from typing import List
+from BaseController import Controller
 
 class SerialObj:
     """
@@ -16,6 +17,7 @@ class SerialObj:
     def __init__(self):
         self.comport: Comport
         self.serialObj: serial.Serial = serial.Serial()
+        self.controller: Controller = None
 
     def available_comports(self) -> List[str]:
         """
